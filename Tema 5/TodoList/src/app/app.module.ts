5facfb9f2a9b8ed9c77d2422add8c0c19a92c5df
Oprisoru-Adelina <adelina.oprisoru@gmail.com>
2023-02-02 14:18:33 +0200

    commit

---
+++ Tema 5/TodoList/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './components/first/first.component'
+import { SecondComponent } from './components/second/second.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
